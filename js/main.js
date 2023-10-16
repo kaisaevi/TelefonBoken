729115dfbeb74dae5721ee2e35dfea44f28161b5
Kaisa Evikoski <kaisa.evikoski@gmail.com>
2023-10-16 11:29:44 +0200

    tweak

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-let inputName = document.querySelector('#add-name');
-let inputNumber = document.querySelector('#add-mobile-number');
+const inputName = document.querySelector('#add-name');
+const inputNumber = document.querySelector('#add-mobile-number');
 
-let errorMessage = document.querySelector('#error-message');
-let addBtn = document.querySelector('#add-contact-btn');
-let removeListBtn = document.querySelector('#remove-contactlist-btn');
-let contactList = document.querySelector('#contact-list');
+const errorMessage = document.querySelector('#error-message');
+const addBtn = document.querySelector('#add-contact-btn');
+const removeListBtn = document.querySelector('#remove-contactlist-btn');
+const contactList = document.querySelector('#contact-list');
 
-let inputContactName = inputName.value;
-let inputContactNumber = inputNumber.value;
+const inputContactName = inputName.value;
+const inputContactNumber = inputNumber.value;
 
 removeListBtn.addEventListener('click', function(){
     contactList.innerHTML = "";
 });
-
+//Funktion för att error-meddelandet
 function errorController(inputContactName, inputContactNumber) {
     if (inputName.value.trim() === "" || isNaN(inputNumber.value) || inputNumber.value.trim() === "") {
                 errorMessage.style.display = "block";
@@ -27,22 +27,22 @@ function addContactToList() {
     if (errorController()) {
         return;
     }
-    let contactItem = document.createElement('li');
-    let container = document.createElement('div');
-    let item = document.createElement('div');
+    const contactItem = document.createElement('li');
+    const container = document.createElement('div');
+    const item = document.createElement('div');
     item.className = 'divItem';
     
-    let inputContactName = inputName.value;
-    let inputContactNumber = inputNumber.value;
+    const inputContactName = inputName.value;
+    const inputContactNumber = inputNumber.value;
 
-    let changeBtn = document.createElement('button');
+    const changeBtn = document.createElement('button');
     changeBtn.textContent = 'Ändra';
     changeBtn.setAttribute('knappState', 'Ändra');
     
-    let removeBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Radera';
 
-    let contactInfo = `Namn: ${inputContactName}, Nummer: ${inputContactNumber}`;
+    const contactInfo = `Namn: ${inputContactName}, Nummer: ${inputContactNumber}`;
     item.textContent = contactInfo;
     
     container.append(item, changeBtn, removeBtn);
@@ -54,18 +54,18 @@ function addContactToList() {
 
         if (e.target.getAttribute('knappState') === 'Ändra') {
             e.target.setAttribute('knappState', 'Spara');
-            let divItem = e.target.parentNode.querySelector('.divItem');
-            let info = divItem.innerHTML;
-            let infoArray = info.split(" ");
-            let number = infoArray[3];
-            let name = infoArray[1].slice(0, -1);
+            const divItem = e.target.parentNode.querySelector('.divItem');
+            const info = divItem.innerHTML;
+            const infoArray = info.split(" ");
+            const number = infoArray[3];
+            const name = infoArray[1].slice(0, -1);
 
-            let nameInput = document.createElement('input');
+            const nameInput = document.createElement('input');
             nameInput.type = 'text';
             nameInput.value = name;
             nameInput.className = 'nameInput';
     
-            let numberInput = document.createElement('input');
+            const numberInput = document.createElement('input');
             numberInput.type = 'text';
             numberInput.value = number;
             numberInput.className = 'numberInput';
@@ -75,13 +75,13 @@ function addContactToList() {
 
         } else {
             e.target.setAttribute('knappState', 'Ändra');
-            let divItem = e.target.parentNode.querySelector('.divItem');
-            let nameInput = divItem.querySelector('.nameInput');
-            let numberInput = divItem.querySelector('.numberInput');
-            let name = nameInput.value;
-            let number = numberInput.value;
+            const divItem = e.target.parentNode.querySelector('.divItem');
+            const nameInput = divItem.querySelector('.nameInput');
+            const numberInput = divItem.querySelector('.numberInput');
+            const name = nameInput.value;
+            const number = numberInput.value;
             if (name === "" || number === "" || isNaN(numberInput.value)) {
-                let emptyValueMessage = document.createElement('div');
+                const emptyValueMessage = document.createElement('div');
                 emptyValueMessage.textContent = "Du kan inte skapa tomma kontakt eller ange ogiltiga nummer.";
                 emptyValueMessage.setAttribute('id', 'invalid-value-message');
                 divItem.appendChild(emptyValueMessage);
@@ -95,6 +95,10 @@ function addContactToList() {
         removeBtn.addEventListener('click', function(e) {
             e.target.parentNode.parentNode.remove(); 
         });
+
+    //Tömmer inputfälten när användaren har lagt till en kontakt 
+    inputName.value = '';
+    inputNumber.value = '';
 };
 
 addBtn.addEventListener('click', addContactToList);
